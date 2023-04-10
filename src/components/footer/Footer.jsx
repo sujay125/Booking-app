a762e84c4645e8368cd23fd851fd5c65f1d856cc
Sujay <sujayawale125@gmail.com>
2023-04-11 01:04:44 +0530

    First commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className="fLists">
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">Districts</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Homes</li>
+                <li className="fListItem">Apartments</li>
+                <li className="fListItem">Resorts</li>
+                <li className="fListItem">Villas</li>
+                <li className="fListItem">Hostels</li>
+                <li className="fListItem">Guest houses</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Unique places to stay</li>
+                <li className="fListItem">Reviews</li>
+                <li className="fListItem">Unpackeed: travel articles</li>
+                <li className="fListItem">Travel communities</li>
+                <li className="fListItem">Seasonal and holoday deals</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Car rentals</li>
+                <li className="fListItem">Flight Finder</li>
+                <li className="fListItem">Restaurant reservations</li>
+                <li className="fListItem">Travel Agents</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Customer Service</li>
+                <li className="fListItem">Partner Help</li>
+                <li className="fListItem">Careers</li>
+                <li className="fListItem">Sustainability</li>
+                <li className="fListItem">Press center</li>
+                <li className="fListItem">Safety Resources Center</li>
+                <li className="fListItem">Investor relations</li>
+                <li className="fListItem">Terms & Conditions</li>
+            </ul>
+        </div>
+        <div className="fText">Copyright @ 2022 Lamabooking.</div>
+    </div>
+  )
+}
+
+export default Footer
